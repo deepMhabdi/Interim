6818fda283127fb86e112ab02f54b1a20b32be29
Ashton <ashtondsouza192@gmail.com>
2025-12-07 23:38:06 +0530

    Structed the subcomponent and awards section is done

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -17,11 +17,24 @@ export default function Awards() {
           RECOGNITIONS WE GOT
         </h1>
       </div>
-      <h1 className=" ml-85 mt-5 mb-10   text-[14vw] tracking-tighter text-[#fcfdee] font-semibold">
+      <h1 className=" ml-85 mt-5    text-[14vw] tracking-tighter text-[#fcfdee] font-semibold">
         Awards
       </h1>
 
-      <Badge />
+      <div className="flex flex-col gap-3">
+        <div className="ml-[200px] flex gap-4">
+          <Badge />
+          <Badge />
+          <Badge />
+        </div>
+        <div className="flex gap-4 ml-[340px]">
+          <Badge />
+          <Badge />
+        </div>
+        <div className="flex gap-4 ml-[500px]">
+          <Badge />
+        </div>
+      </div>
     </div>
   );
 }
