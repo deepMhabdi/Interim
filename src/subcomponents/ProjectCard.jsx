81611634eaf3ccba8eb0742b4038721b2d19d09e
Ashton <ashtondsouza192@gmail.com>
2025-12-10 16:02:44 +0530

    project card done

--- src/subcomponents/ProjectCard.jsx
+++ src/subcomponents/ProjectCard.jsx
@@ -1,14 +1,18 @@
-export default function ProjectCard({firstButton, secondButton, imgurl , heading ,text}) {
+export default function ProjectCard({ firstButton, secondButton, imgurl, heading, text }) {
     return (
         <>
-            <div className="w-[1200px] h-[420px] rounded-xl p-5 bg-[#f6f7e9] flex gap-7 ">
-            <div className="flex gap-2 items-start ">
-                <button className="px-1 py-1 uppercase rounded-lg h-[30px] w-[100px] border border-[#1e3730]   text-[#1e3730] inter-nav text-[0.8rem] ">{secondButton}</button>
-                <button className="px-1 py-1 uppercase rounded-lg h-[30px] w-[100px] border border-[#1e3730]   text-[#1e3730] inter-nav text-[0.8rem] ">{firstButton}</button>
-            </div>
-            <div>
-                <img src={imgurl} alt="" className="rounded-xl w-[350px] h-full object-cover" />
-            </div>
+            <div className="w-[1300px] h-[420px] rounded-xl p-5 bg-[#f6f7e9] flex ">
+                <div className="flex gap-2 items-start ">
+                    <button className="px-1 py-1 uppercase rounded-lg h-[30px] w-[100px] border border-[#1e3730]   text-[#1e3730] inter-nav text-[0.8rem] ">{secondButton}</button>
+                    <button className="px-1 py-1 uppercase rounded-lg h-[30px] w-[100px] border border-[#1e3730]   text-[#1e3730] inter-nav text-[0.8rem] ">{firstButton}</button>
+                </div>
+                <div className="w-full h-full ml-10">
+                    <img src={imgurl} alt="" className="rounded-xl w-[350px] h-full object-cover" />
+                </div>
+                <div className="flex flex-col items-start justify-between -ml-[150px]">
+                    <h1 className="inter  text-[#1e3730] text-[1.8rem] leading-none">{heading}</h1>
+                    <p className="inter-nav text-[#1e3730]  leading-none">{text}</p>
+                </div>
             </div>
         </>
     )
