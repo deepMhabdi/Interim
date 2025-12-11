532cfe141ab6ff53492b6a1339f7e89dc7da3e6b
Ashton <ashtondsouza192@gmail.com>
2025-12-11 12:50:25 +0530

    header section done

--- src/subcomponents/BlogCard.jsx
+++ src/subcomponents/BlogCard.jsx
@@ -1,3 +1,5 @@
+import { time } from "framer-motion";
+
 export default function BlogCard({ imgurl, tag, date, timeToRead, heading, }) {
     return (
         <>
@@ -9,8 +11,14 @@ export default function BlogCard({ imgurl, tag, date, timeToRead, heading, }) {
                             <img src={imgurl} alt="" className="w-full h-full object-cover   rounded-xl transition-transform duration-700 ease-in group-hover:scale-110 " />
                         </div>
                 <div className="flex flex-col justify-start">
-                    <div className="flex">
-                        <div className="rounded-full w-fit px-3 py-1 h-[25px] bg-[#f3ad84] mt-7 ml-1 inter-nav text-[#1d332c] text-[0.7rem] font-semibold uppercase">{tag}</div>
+                    <div className="flex gap-2 mt-7 ml-1 items-center justify-center">
+                        <div className="rounded-full w-fit px-3 py-1 h-[25px] bg-[#f3ad84]  inter-nav text-[#1d332c] text-[0.7rem] font-semibold uppercase">{tag}</div>
+                        <div className="w-[6px]  h-[5px] bg-black rounded-full ">
+                        </div> 
+                            <div className="inter-nav text-[0.8rem] tracking-tight whitespace-nowrap text-[#1d332c]">{date}</div>
+                        <div className="w-[6px]  h-[5px] bg-black rounded-full ">
+                        </div>
+                            <div className="inter-nav text-[0.8rem] tracking-tight whitespace-nowrap text-[#1d332c] uppercase">{timeToRead} min read</div>
                     </div>
                 </div>
                     </div>
