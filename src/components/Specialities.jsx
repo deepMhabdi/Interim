663331b33d0563d5f2e3498eb94eaf662e6fe59f
Ashton <ashtondsouza192@gmail.com>
2025-12-11 22:10:22 +0530

    recent changes

--- src/components/Specialities.jsx
+++ src/components/Specialities.jsx
@@ -1,30 +1,57 @@
 
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, CircleStopIcon } from "lucide-react";
 export default function Specialities() {
     return (
-        <div>
-            <div className="flex gap-2 h-screen ">
+        <div className="">
+            <div className="flex gap-5 min-h-screen  ">
                 <div className="relative  ">
                     <img src="https://framerusercontent.com/images/KGTXISgxyxmkCq256EXQwsbnNto.jpg" alt="" className="w-[620px] h-[640px] object-cover rounded-xl" />
 
-                    <div className="absolute bottom-0 left-0 -translate-y-26  w-full px-2">
-                        <div className="w-full  rounded-xl backdrop-blur-md bg-black/10 px-3 py-4 ">
-                                <h1 className="text-white inter-nav leading-none  text-[2.5rem]  whitespace-nowrap">Our speciality lies in transforming <br />spaces into harmonious <br />environments</h1>
-                                <p className="text-white inter-nav leading-normal text-[1rem] mt-5">
-                                    Our craft, a delicate dance of light and shadow, seeks to conjure spaces that echo with the whispers of timeless elegance and bespoke charm.
-                                </p>
-                                {/* button */}
-                                <div className="flex gap-1 items-center mt-10">
-                                    <div className="text-[#15372b] bg-white text-center font-semibold rounded-2xl  py-3  w-[150px] inter-nav">
-                                        Start a Project
-                                    </div>
-                                    <div className="p-2 w-[50px] h-[50px] bg-[#f3ad84] rounded-xl flex items-center justify-center">
-                                        <ArrowRight color="#4b5a54" />
-                                    </div>
+                    <div className="absolute bottom-0 left-0 -translate-y-110  w-full px-2">
+                        <div className="w-ful    rounded-xl backdrop-blur-md bg-black/10 px-3 py-4 ">
+                            <h1 className="text-white inter-nav leading-none  text-[2.5rem]  whitespace-nowrap">Our speciality lies in transforming <br />spaces into harmonious <br />environments</h1>
+                            <p className="text-white inter-nav leading-normal text-[1rem] mt-5">
+                                Our craft, a delicate dance of light and shadow, seeks to conjure spaces that echo with the whispers of timeless elegance and bespoke charm.
+                            </p>
+                            {/* button */}
+                            <div className="flex gap-1 items-center mt-10">
+                                <div className="text-[#15372b] bg-white text-center font-semibold rounded-2xl  py-3  w-[150px] inter-nav">
+                                    Start a Project
+                                </div>
+                                <div className="p-2 w-[50px] h-[50px] bg-[#f3ad84] rounded-xl flex items-center justify-center">
+                                    <ArrowRight color="#4b5a54" />
                                 </div>
                             </div>
+                        </div>
                     </div>
 
+                </div>
+
+                <div className="flex flex-col justify-start items-start h-fit ">
+                    <div className="flex gap-1  items-center  justify-center">
+                        <CircleStopIcon className="w-3 h-3 mb-[0.2rem]" />
+                        <h1 className=" text-[#1e3730] text-[0.9rem] font-bold tracking-wide uppercase">our specialities</h1>
+
+                    </div>
+                    <div className="inter text-[#1e3730] ">
+                        <h1 className="whitespace-nowrap text-5xl mt-2 ">Why you'll love working with <br />Interim</h1>
+                    </div>
+                    <div className="rounded-xl w-[700px] h-fit bg-[#f6f7e9] p-4 mt-5 text-[#1e3730] inter-nav">
+                        <p>"Design is more than just aesthetics; it's a reflection of the soul and a journey towards creating timeless spaces that resonate with individuality and purpose. At Interim, we strive to craft environments that not only captivate the eye but also inspire the mind and nurture the spirit."</p>
+                        <div className="flex gap-5 mt-5">
+                            <div>
+                                <img src="https://framerusercontent.com/images/XDcRjEJ0HdfRgvjaqQFTeR8Bsk.jpg?scale-down-to=1024" alt="" className="w-[70px] h-[70px] rounded-xl" />
+                            </div>
+                            <div className="flex flex-col -ml-2 mt-1">
+                                <h1 className="inter-nav text-[1.2rem]">Real Mehdi</h1>
+                                <h1 className="text-[#495853] inter-nav">Founder,Interim Design Studio</h1>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="bg-[#1e3730] h-[700px] w-[700px] rounded-xl mt-2  ">
+
+                    </div>
                 </div>
 
             </div>
