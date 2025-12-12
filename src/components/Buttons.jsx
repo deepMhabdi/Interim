4cc4be621a9ce35881360079063b496dae10ae33
Ashton <ashtondsouza192@gmail.com>
2025-12-12 14:36:34 +0530

    Implemented conditional rendering if href passed then a tag o/w normal ,

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,17 +1,21 @@
 import { ArrowRight } from "lucide-react";
 
 const Buttons = ({ title, onClick, redirectUrl }) => {
-  return (
-    <a href={redirectUrl}>
-      <div className="flex gap-1 items-center group  w-fit group-hover:gap-0" onClick={onClick}>
-        <div className="bg-[#15372b] text-white text-center font-semibold rounded-2xl  py-3  w-[150px] inter-nav group-hover:-mr-0.5 transition-all duration-300 ease-in group-hover:rounded-br-none ">
-          {title}
-        </div>
-        <div className="p-2 w-[50px] h-[50px] bg-[#f3ad84] rounded-xl flex items-center mb-px justify-center group-hover:-ml-0.5 transition-all duration-300 ease-in group-hover:rounded-bl-none">
-          <ArrowRight color="#4b5a54" />
-        </div>
+  const content = (
+    <div className="flex gap-1 items-center group w-fit group-hover:gap-0" onClick={onClick}>
+      <div className="bg-[#15372b] text-white text-center font-semibold rounded-2xl py-3 w-[150px] inter-nav group-hover:-mr-0.5 transition-all duration-300 ease-in group-hover:rounded-br-none">
+        {title}
       </div>
-    </a>
+      <div className="p-2 w-[50px] h-[50px] bg-[#f3ad84] rounded-xl flex items-center mb-px justify-center group-hover:-ml-0.5 transition-all duration-300 ease-in group-hover:rounded-bl-none">
+        <ArrowRight color="#4b5a54" />
+      </div>
+    </div>
+  );
+
+  return redirectUrl ? (
+    <a href={redirectUrl}>{content}</a>
+  ) : (
+    <div className="cursor-pointer">{content}</div>
   );
 };
 
