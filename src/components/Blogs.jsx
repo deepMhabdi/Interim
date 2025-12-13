75fde2064d5a8c137e4e669d5a28730dcffab856
ashton dsouza <ashtondsouza192@gmai.com>
2025-12-13 15:45:08 +0530

    Recent changes

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -3,11 +3,11 @@ import BlogCard from '../subcomponents/BlogCard';
 export default function Blogs() {
   return (
     <>
-      <div className=" bg-[#f7f6e9] relative min-h-screen w-full mt-20 pt-1 pb-1">
+      <div className=" bg-[#f7f6e9] relative min-h-screen w-full  pt-1 pb-1">
 
-        <h1 className="text-[13rem]  interServices text-center  tracking-tight text-[#1e3730] mt-20 ">Blogs</h1>
-        <div className="flex gap-1 absolute top-0 left-1/2 -translate-x-15 translate-y-43 items-center  justify-center">
-          <CircleStopIcon className="w-3 h-3 mb-[0.2rem]" />
+        <h1 className="md:text-[13rem] text-[9rem]  interServices text-center  tracking-tight text-[#1e3730] md:mt-20  ">Blogs</h1>
+        <div className="flex gap-1 absolute top-0 left-1/2 -translate-x-12 md:translate-y-43 translate-y-16 items-center  justify-center">
+          <CircleStopIcon className="w-3 h-3 mb-[0.1rem]" />
           <h1 className=" text-[#1e3730] text-[0.9rem] font-bold tracking-wide uppercase">articles and insights</h1>
 
         </div>
